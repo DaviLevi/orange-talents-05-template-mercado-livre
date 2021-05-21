9cf72a8a2b260106e8126db32150b9a3a9645dff
DaviLevi <davide.sgalambro@zup.com.br>
2021-05-21 10:00:15 -0300

    0.0.0 setup

---
+++ src/test/java/br/com/zup/ot5/fase3/mercadolivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.ot5.fase3.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

67a905a069c4a70900918278d3261cb32f0b5e6a
DaviLevi <davide.sgalambro@zup.com.br>
2021-05-26 15:13:55 -0300

    Ajuste logica de montagem url redirecionamento

--- src/main/java/br/com/zup/ot5/fase3/mercadolivre/compra_produto_parte_1/ResolvedorTransacaoPagseguro.java
+++ src/main/java/br/com/zup/ot5/fase3/mercadolivre/compra_produto_parte_1/ResolvedorTransacaoPagseguro.java
@@ -2,11 +2,11 @@ package br.com.zup.ot5.fase3.mercadolivre.compra_produto_parte_1;
 
 public class ResolvedorTransacaoPagseguro implements ResolvedorTransacao{
 
-	private static final String URL_AFTER_PAYMENT = "http://mercadolivre.com.br/home?depoisPagamento=true&gateway=pagseguro";
+	private static final String URL_AFTER_PAYMENT = "/retorno-pagseguro/%d";
 	
 	@Override
 	public String resolva(Long idCompra) {
-		return String.format("pagseguro.com?buyerId={%s}&redirectUrl={%s}", idCompra, URL_AFTER_PAYMENT);
+		return String.format("pagseguro.com?buyerId=%s&redirectUrl=%s", idCompra, String.format(URL_AFTER_PAYMENT, idCompra));
 	}
 
 }
--- src/main/java/br/com/zup/ot5/fase3/mercadolivre/compra_produto_parte_1/ResolvedorTransacaoPaypal.java
+++ src/main/java/br/com/zup/ot5/fase3/mercadolivre/compra_produto_parte_1/ResolvedorTransacaoPaypal.java
@@ -2,11 +2,11 @@ package br.com.zup.ot5.fase3.mercadolivre.compra_produto_parte_1;
 
 public class ResolvedorTransacaoPaypal implements ResolvedorTransacao{
 
-	private static final String URL_AFTER_PAYMENT = "http://mercadolivre.com.br/home?depoisPagamento=true&gateway=paypal";
+	private static final String URL_AFTER_PAYMENT = "/retorno-paypal/%d";
 	
 	@Override
 	public String resolva(Long idCompra) {
-		return String.format("paypal.com?buyerId={%s}&redirectUrl={%s}", idCompra, URL_AFTER_PAYMENT);
+		return String.format("paypal.com?buyerId=%s&redirectUrl=%s", idCompra, String.format(URL_AFTER_PAYMENT, idCompra));
 	}
 
 }
